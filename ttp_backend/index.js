9c9f114cdd92b315819dcbbfba24b4faaf117928
Danny Lee <dreamer@panix.com>
2020-02-11 23:52:09 -0500

    Validated user login and token passing is working properly

--- ttp_backend/index.js
+++ ttp_backend/index.js
@@ -1,4 +1,27 @@
-window.fetch('http://localhost:3000/api/v1/users', {
+// Create a user fetch
+// 
+// window.fetch('http://localhost:3000/api/v1/users', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     Accept: 'application/json'
+//   },
+//   body: JSON.stringify({
+//     user: {
+//       username: "danny",
+//       password: "password",
+//       email: "danny@email.com"
+//     }
+//   })
+// })
+//   .then(r => r.json())
+//   .then(console.log)
+
+// Login a user and get a token
+
+let userToken = ''
+
+window.fetch('http://localhost:3000/api/v1/login', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
@@ -6,11 +29,26 @@ window.fetch('http://localhost:3000/api/v1/users', {
   },
   body: JSON.stringify({
     user: {
-      username: "danny",
-      password: "password",
-      email: "danny@email.com"
+      username: 'danny',
+      password: 'password',
     }
   })
 })
   .then(r => r.json())
-  .then(console.log)
+  .then(token => {
+    userToken = token.jwt
+    console.log('token: ', userToken)
+  })
+  .then(r => {
+    window.fetch('http://localhost:3000/api/v1/profile', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${userToken}`
+      }
+    })
+      .then(r => r.json())
+      .then(u => {
+        console.log(u.user)
+      })
+
+  })
