98f433bc48e67ee7eb7b8c9851f60521d96e8da1
Danny Lee <dreamer@panix.com>
2020-02-12 14:25:29 -0500

    Switch to sessions for logging in

--- ttp_backend/app/models/user.rb
+++ ttp_backend/app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, uniqueness: { case_sensitive: false }
 
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :username, length: { minimum: 4 }
+  validates :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   has_many :stocks
 end
--- ttp_backend/config/routes.rb
+++ ttp_backend/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create]
-      post '/login', to: 'auth#create'
-      get '/profile', to: 'users#profile'
+      post '/login', to: 'sessions#create'
+      delete '/logout', to: 'sessions#destroy'
+      get '/logged_in', to: 'sessions#is_logged_in?'
+      
+      resources :users, only: [:create, :show, :index]
     end
   end
 end
